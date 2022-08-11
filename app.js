5d26f668e40b8d2c28727d94ab4ce2f8c672135a
ahmadmarhaba <dragon.8881@hotmail.com>
2022-08-11 18:52:03 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const server = require('http').Server(app)
+const io = require("socket.io")(server,
+  {
+    cors: {
+      origin: "http://localhost",
+      methods: ["GET", "POST"]
+    }
+  }
+  ); 
+  
+  function serverLog(text) {
+    console.log("Web Node Server =>", text);
+  }
+  
+  server.listen(process.env.PORT, () => serverLog(`Listening on port ${process.env.PORT}`));
+  
+
+io.on('connection', function (socket) {
+  serverLog("Connection Started ( Socket id: " + socket.id + " )")
+})
